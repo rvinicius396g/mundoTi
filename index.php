bc1ad7ef0e2b73a35ee2b40a8e41a2d27217dee7
rvinicius <roberto396g@hotmail.com>
2016-02-24 19:41:25 -0300

    website

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+	<title>Home</title>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#barranavegacao">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>						
+					</button>
+					<a href="#" class="navbar-brand">Mundo Ti</a>
+				</div>
+
+				<div class="collapse navbar-collapse" id="barranavegacao">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Inicio</a></li>
+						<li><a href="#">Contato</a></li>
+						<li><a href="#">Sobre</a></li>
+					</ul>
+				</div>
+			</div><!--END !!Container-Fluid-->
+		</nav>
+	</div>	
+</body>
+</html>
