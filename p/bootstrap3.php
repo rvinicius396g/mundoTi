41e70bd006f386cc76ba5915799623225553e6e4
rvinicius <roberto396g@hotmail.com>
2016-03-06 16:56:30 -0300

    insert btn-default

--- p/bootstrap3.php
+++ p/bootstrap3.php
@@ -7,7 +7,7 @@
 <p style="text-indent: 35px;font-size: 12pt;text-align: justify;">
 	O intuito dessas videos aulas, foram de ensinar a você criar um site responsivo na prática e não só ficar no conceito ou introdução, então é normal algumas coisas ficarem sobrando ou por fora, então é fundamentalmente crucial que vocês tenham a curiosidade de acessarem ou fazer o download de conteúdos extras, pois você terá uma base maior de conhecimento para o seu aprendizado.
 </p>
-<button data-toggle="collapse" data-target="#aula0">Aula 0 - BootStrap Introdução</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula0">Aula 0 - BootStrap Introdução</button>
 <div id="aula0" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/8zYYaeR3La4" frameborder="0" allowfullscreen style="display:block;margin: 0 auto;" ></iframe>
@@ -27,7 +27,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula1">Aula 1 - Conhecendo o BootStrap</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula1">Aula 1 - Conhecendo o BootStrap</button>
 <div id="aula1" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/1HcO0HSoOgo" frameborder="0" allowfullscreen style="display:block;margin: 0 auto;"></iframe>
@@ -47,7 +47,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula2">Aula 2 - Preparando Nossa área de Trabalho</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula2">Aula 2 - Preparando Nossa área de Trabalho</button>
 <div id="aula2" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/Yr4iczE3veY" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
@@ -67,7 +67,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula3">Aula 3 - Desenvolvendo um grid basico</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula3">Aula 3 - Desenvolvendo um grid basico</button>
 <div id="aula3" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/-1MNf4XMVPs" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
@@ -87,7 +87,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula5">Aula 5 - Criando a área da logo (Jumbotron) </button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula5">Aula 5 - Criando a área da logo (Jumbotron) </button>
 <div id="aula5" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/kIUf2qGWkgo" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
@@ -107,7 +107,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula6">Aula 6 - Trabalhando com col e row para criar a área dos artigos</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula6">Aula 6 - Trabalhando com col e row para criar a área dos artigos</button>
 <div id="aula6" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/QNf7Gl3aO_I" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
@@ -127,7 +127,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula7">Aula 7 - Criando o Footer</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula7">Aula 7 - Criando o Footer</button>
 <div id="aula7" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/2lVtUAhqHq0" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
@@ -147,7 +147,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula8">Aula 8 - Inserindo Conteúdo no site</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula8">Aula 8 - Inserindo Conteúdo no site</button>
 <div id="aula8" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/HOl_Olv1Hao" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
@@ -167,7 +167,7 @@
 	</figure>
 </div>
 
-<button data-toggle="collapse" data-target="#aula9">Aula 9 - Estilizando o site</button>
+<button class="btn btn-info" data-toggle="collapse" data-target="#aula9">Aula 9 - Estilizando o site</button>
 <div id="aula9" class="collapse">
 	<figure style="background-color:black;">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/UBk34gDy4F0" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
