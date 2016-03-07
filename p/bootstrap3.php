7f87cdb984e6be73ce4cb1fdc3f2c8f695e00e2d
rvinicius <roberto396g@hotmail.com>
2016-03-07 19:39:20 -0300

    Responsive Embeds

--- p/bootstrap3.php
+++ p/bootstrap3.php
@@ -19,8 +19,8 @@
 </p>
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula0">Aula 0 - BootStrap Introdução</button>
 <div id="aula0" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/8zYYaeR3La4" frameborder="0" allowfullscreen style="display:block;margin: 0 auto;" ></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8zYYaeR3La4" frameborder="0" allowfullscreen ></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -39,8 +39,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula1">Aula 1 - Conhecendo o BootStrap</button>
 <div id="aula1" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/1HcO0HSoOgo" frameborder="0" allowfullscreen style="display:block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/1HcO0HSoOgo" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -59,8 +59,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula2">Aula 2 - Preparando Nossa área de Trabalho</button>
 <div id="aula2" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/Yr4iczE3veY" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/Yr4iczE3veY" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -79,8 +79,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula3">Aula 3 - Desenvolvendo um grid basico</button>
 <div id="aula3" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/-1MNf4XMVPs" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-1MNf4XMVPs" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -99,8 +99,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula5">Aula 5 - Criando a área da logo (Jumbotron) </button>
 <div id="aula5" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/kIUf2qGWkgo" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/kIUf2qGWkgo" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -119,8 +119,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula6">Aula 6 - Trabalhando com col e row para criar a área dos artigos</button>
 <div id="aula6" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/QNf7Gl3aO_I" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/QNf7Gl3aO_I" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -139,8 +139,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula7">Aula 7 - Criando o Footer</button>
 <div id="aula7" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/2lVtUAhqHq0" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/2lVtUAhqHq0" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -159,8 +159,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula8">Aula 8 - Inserindo Conteúdo no site</button>
 <div id="aula8" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/HOl_Olv1Hao" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/HOl_Olv1Hao" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -179,8 +179,8 @@
 
 <button class="btn btn-info" data-toggle="collapse" data-target="#aula9">Aula 9 - Estilizando o site</button>
 <div id="aula9" class="collapse">
-	<figure style="background-color:black;">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/UBk34gDy4F0" frameborder="0" allowfullscreen style="display: block;margin: 0 auto;"></iframe>
+	<figure class="embed-responsive embed-responsive-16by9" style="background-color:black;">
+		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/UBk34gDy4F0" frameborder="0" allowfullscreen></iframe>
 		<figcaption style="background-color: white;">
 			<h4>Informações Importantes;</h4><hr/>
 			►| - BootStrap Link;	
@@ -198,6 +198,7 @@
 </div>
 <div id="fb-root"></div>
 <!-- COMENTÁRIOS NO FACEBOOK EM RELACAO AO BOOTSTRAP -->
+<div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -205,5 +206,6 @@
   js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+
 <div class="fb-comments" data-href="http://mundoti.site88.net/index.php?cont=bootstrap" data-width="100%" data-numposts="8"></div>
 <!-- COMENTÁRIOS NO FACEBOOK EM RELACAO AO BOOTSTRAP -->
